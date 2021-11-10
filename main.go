153c673b69a4f937e6cfda34701b46996bcd7c3a
Shingi <shingirai.chanakira@partner.bmw.co.za>
2021-11-10 09:59:33 +0200

    feat: split file into smaller components

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math"
 	"os"
+	"strconv"
 )
 
 var fileDir string = "C:\\Users\\QXZ1TD8\\Desktop\\SuRE\\working_dir\\data.txt"
 
-var outputDir = "C:\\Users\\QXZ1TD8\\Desktop\\SuRE\\working_dir\\output_dir"
+var outputDir = "C:\\Users\\QXZ1TD8\\Desktop\\SuRE\\working_dir\\output_dir\\"
 
 func main() {
 	splitFile()
@@ -34,5 +36,24 @@ func splitFile() {
 	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
 
 	fmt.Printf("Spliting into %d parts", totalPartsNum)
+
+	for i := uint64(0); i <= totalPartsNum; i++ {
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partBuffer := make([]byte, partSize)
+
+		file.Read(partBuffer)
+
+		fileName := outputDir + "data_" + strconv.FormatUint(i, 10) + ".txt"
+		_, err := os.Create(fileName)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)
+
+		fmt.Println("Split to : ", fileName)
+	}
 
 }
