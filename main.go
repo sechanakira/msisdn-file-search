4d03f86b135619b1d52bf8e5a182cfe70a9638b0
Shingi <shingirai.chanakira@partner.bmw.co.za>
2021-11-19 09:26:37 +0200

    feat: persitance

--- main.go
+++ main.go
@@ -9,30 +9,35 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 const (
 	layout    = "2006-01-02 15:04:05"
 	fileDir   = "C:\\Users\\QXZ1TD8\\Desktop\\SuRE\\working_dir\\data.txt"
 	outputDir = "C:\\Users\\QXZ1TD8\\Desktop\\SuRE\\working_dir\\output_dir\\"
+	dsn       = "root:changeit@tcp(127.0.0.1:3306)/gorm_start?charset=utf8mb4&parseTime=True&loc=Local"
 )
 
-type subscriberData struct {
-	msisdn          string
-	countoforigin   string
-	dob             time.Time
-	dod             time.Time
-	firstname       string
-	lastname        string
-	issuingCountry  string
-	registrarstatus string
-	status          string
-	postaladdress   string
-	physicaladdress string
-	title           string
-	companyName     string
-	gender          string
-	occupation      string
+type SubscriberData struct {
+	gorm.Model
+	Msisdn          string
+	CountOfOrigin   string
+	Dob             time.Time
+	Dod             time.Time
+	FirstName       string
+	LastName        string
+	IssuingCountry  string
+	RegistrarStatus string
+	Status          string
+	PostalAddress   string
+	PhysicalAddress string
+	Title           string
+	CompanyName     string
+	Gender          string
+	Occupation      string
 }
 
 type searchResult struct {
@@ -42,6 +47,7 @@ type searchResult struct {
 }
 
 func main() {
+	readAllAndSave()
 	entries, err := os.ReadDir(outputDir)
 
 	if err != nil {
@@ -58,12 +64,120 @@ func main() {
 	fmt.Println(sr)
 }
 
-func reaAllAndSave(){
-	;
+func readAllAndSave() {
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	sql, _ := db.DB()
+
+	defer sql.Close()
+
+	db.AutoMigrate(&SubscriberData{})
+
+	entries, err := os.ReadDir(outputDir)
+
+	if err != nil {
+		log.Fatalln("Failed to open directory ", outputDir)
+	}
+
+	if len(entries) == 0 {
+		splitFile()
+	}
+
+	c := make(chan bool)
+
+	for _, entry := range entries {
+		go readAndSaveFileContents(entry.Name(), c, db)
+	}
+
+	for i := 0; i <= len(entries); i++ {
+		fmt.Println(<-c)
+	}
 }
 
-func msisdnSearch(msisdn string) (subscriberData, error) {
-	sb := subscriberData{}
+func readAndSaveFileContents(fileName string, c chan bool, db *gorm.DB) {
+	bs, _ := os.ReadFile(outputDir + fileName)
+
+	fileContents := string(bs)
+
+	lines := strings.Split(fileContents, "\r\n")
+
+	for _, line := range lines {
+		if len(line) != 0 && line != "" {
+			sb := SubscriberData{}
+
+			line = strings.ReplaceAll(line, `"`, "")
+			data := strings.Split(line, "|")
+
+			sb.Msisdn = data[0]
+
+			if len(data) > 1 {
+				sb.CountOfOrigin = data[1]
+			}
+
+			if len(data) > 2 {
+				dob, _ := time.Parse(layout, data[2])
+				sb.Dob = dob
+			}
+
+			if len(data) > 3 {
+				dod, _ := time.Parse(layout, data[3])
+				sb.Dod = dod
+			}
+
+			if len(data) > 4 {
+				sb.FirstName = data[4]
+			}
+
+			if len(data) > 5 {
+				sb.LastName = data[5]
+			}
+
+			if len(data) > 6 {
+				sb.IssuingCountry = data[6]
+			}
+
+			if len(data) > 7 {
+				sb.RegistrarStatus = data[7]
+			}
+
+			if len(data) > 8 {
+				sb.Status = data[8]
+			}
+
+			if len(data) > 9 {
+				sb.PostalAddress = data[9]
+			}
+
+			if len(data) > 10 {
+				sb.PhysicalAddress = data[10]
+			}
+
+			if len(data) > 11 {
+				sb.Title = data[11]
+			}
+
+			if len(data) > 13 {
+				sb.CompanyName = data[13]
+			}
+
+			if len(data) > 14 {
+				sb.Gender = data[14]
+			}
+
+			if len(data) > 15 {
+				sb.Occupation = data[15]
+			}
+
+			db.Create(&sb)
+		}
+	}
+
+	c <- true
+
+}
+
+func msisdnSearch(msisdn string) (SubscriberData, error) {
+	sb := SubscriberData{}
 
 	dirEntry, err := os.ReadDir(outputDir)
 
@@ -83,25 +197,25 @@ func msisdnSearch(msisdn string) (subscriberData, error) {
 		if sr.found {
 			data := strings.Split(strings.ReplaceAll(sr.content, `"`, ""), "|")
 
-			sb.msisdn = data[0]
-			sb.countoforigin = data[1]
-			sb.firstname = data[4]
-			sb.lastname = data[5]
-			sb.issuingCountry = data[6]
-			sb.registrarstatus = data[7]
-			sb.status = data[8]
-			sb.postaladdress = data[9]
-			sb.physicaladdress = data[10]
-			sb.title = data[11]
-			sb.companyName = data[13]
-			sb.gender = data[14]
-			sb.occupation = data[15]
+			sb.Msisdn = data[0]
+			sb.CountOfOrigin = data[1]
+			sb.FirstName = data[4]
+			sb.LastName = data[5]
+			sb.IssuingCountry = data[6]
+			sb.RegistrarStatus = data[7]
+			sb.Status = data[8]
+			sb.PostalAddress = data[9]
+			sb.PhysicalAddress = data[10]
+			sb.Title = data[11]
+			sb.CompanyName = data[13]
+			sb.Gender = data[14]
+			sb.Occupation = data[15]
 
 			dob, _ := time.Parse(layout, data[2])
 			dod, _ := time.Parse(layout, data[3])
 
-			sb.dob = dob
-			sb.dod = dod
+			sb.Dob = dob
+			sb.Dod = dod
 
 		}
 	}
